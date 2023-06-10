4b090350751938d1ce2ee7575b7d81eaa5fec798
Quang167 <=>
2023-06-10 14:38:33 +0700

    Ex 26

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap'
+import Main from './components/MainComponent';
+// import { Navbar, NavbarBrand } from 'reactstrap'
 // import { Media } from 'reactstrap';
-import Menu from './components/MenuComponent';
+// import Menu from './components/MenuComponent';
 // import NavbarHeader from './components/Navbar';
-import { DISHES } from './shared/dishes';
+// import { DISHES } from './shared/dishes';
 // import ButtonCount from './components/ButtonCount';
 // import ParentComponent from './components/ButtonCount/button';
 
@@ -13,32 +14,50 @@ import { DISHES } from './shared/dishes';
 
 // function App() {
 //   return (
+//     <>
 //     <div className="App">
 //       <NavbarHeader />
-//     <Menu />
+//     {/* <Menu /> */}
 //   {/* <ButtonCount props="Hello"/> */}
 //   {/* <ParentComponent /> */}
 //     </div>
+//     </>
 //   );
 // }
+
+// Ex 25
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       dishes: DISHES
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <Navbar dark color="primary">
+//           <div className="container">
+//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//           </div>
+//         </Navbar>
+//         <Menu dishes={this.state.dishes} />
+//       </div>
+//     );
+//   }
+// }
+// Ex 25
+
+// Ex 26
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
 }
+
 export default App;
