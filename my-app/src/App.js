907fb0f9854504307b61158d9c6410f412c17048
Quang167 <=>
2023-06-12 09:58:53 +0700

    React Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,63 +1,26 @@
 import './App.css';
 import { Component } from 'react';
 import Main from './components/MainComponent';
-// import { Navbar, NavbarBrand } from 'reactstrap'
 // import { Media } from 'reactstrap';
-// import Menu from './components/MenuComponent';
-// import NavbarHeader from './components/Navbar';
-// import { DISHES } from './shared/dishes';
-// import ButtonCount from './components/ButtonCount';
-// import ParentComponent from './components/ButtonCount/button';
+import { DISHES } from './shared/dishes';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 
-// function App() {
-//   return (
-//     <>
-//     <div className="App">
-//       <NavbarHeader />
-//     {/* <Menu /> */}
-//   {/* <ButtonCount props="Hello"/> */}
-//   {/* <ParentComponent /> */}
-//     </div>
-//     </>
-//   );
-// }
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
 
-// Ex 25
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//         <Menu dishes={this.state.dishes} />
-//       </div>
-//     );
-//   }
-// }
-// Ex 25
-
-// Ex 26
-class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Main />
-      </div>
-    );
-  }
+    </>
+  );
 }
+
+
 
 export default App;
